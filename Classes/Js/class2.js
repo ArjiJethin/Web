bc68f5382df26c4008ffc9c99c9055320520190a
Eswar Arji <arjijethin.ac0705@gmail.com>
2024-11-07 11:38:02 +0530

    js practise

--- Classes/Js/class2.js
+++ Classes/Js/class2.js
@@ -1,41 +1,49 @@
 class Day {
-    constructor(name) {
-        this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
+  }
 
-    getName() {
-        return this.name;
-    }
+  getName() {
+    return this.name;
+  }
 
-    nextDay() {
-        const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let currentIndex = daysOfWeek.indexOf(this.name);
-        let nextIndex = (currentIndex + 1) % daysOfWeek.length;
-        return new Day(daysOfWeek[nextIndex]);
-    }
+  nextDay() {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let currentIndex = daysOfWeek.indexOf(this.name);
+    let nextIndex = (currentIndex + 1) % daysOfWeek.length;
+    return new Day(daysOfWeek[nextIndex]);
+  }
 }
 
 class Week {
-    constructor() {
-        this.days = [
-            new Day('Sunday'),
-            new Day('Monday'),
-            new Day('Tuesday'),
-            new Day('Wednesday'),
-            new Day('Thursday'),
-            new Day('Friday'),
-            new Day('Saturday')
-        ];
-        this.currentDayIndex = 0;
-    }
+  constructor() {
+    this.days = [
+      new Day("Sunday"),
+      new Day("Monday"),
+      new Day("Tuesday"),
+      new Day("Wednesday"),
+      new Day("Thursday"),
+      new Day("Friday"),
+      new Day("Saturday"),
+    ];
+    this.currentDayIndex = 0;
+  }
 
-    getCurrentDay() {
-        return this.days[this.currentDayIndex].getName();
-    }
+  getCurrentDay() {
+    return this.days[this.currentDayIndex].getName();
+  }
 
-    moveToNextDay() {
-        this.currentDayIndex = (this.currentDayIndex + 1) % this.days.length;
-    }
+  moveToNextDay() {
+    this.currentDayIndex = (this.currentDayIndex + 1) % this.days.length;
+  }
 }
 
 // Example usage:
